fbfbd556f2824c202d40a6ed08236e809edc5ef1
Leonard Elezi <leonard@WirelessStudenten-143169207182.uantwerpen.be>
2015-06-23 16:24:04 +0200

    Initial Commit

---
+++ CantTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CantTalk
+//
+//  Created by Leonard Elezi on 6/23/15.
+//  Copyright (c) 2015 Leonard Elezi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CantTalk/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CantTalk
+//
+//  Created by Leonard Elezi on 6/23/15.
+//  Copyright (c) 2015 Leonard Elezi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
